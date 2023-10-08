8eff62e6f857238b97ca9fed0bd4a0e289f30e98
Mohammad Sumon <mohammad.sumon9897@gmail.com>
2023-10-08 20:32:44 +0600

    First Commit

---
+++ app/Http/Controllers/admin/AdminLoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminLoginController extends Controller
+{
+    public function index(){
+        return view('admin.login');
+    }
+
+
+    public function  authenticate(Request $request){
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator -> passes()){
+
+        }else{
+            return redirct()->route('admin.login')
+            ->withErrors($validator)
+            ->withInput($request ->only('email'));
+
+        }
+
+    }
+}
